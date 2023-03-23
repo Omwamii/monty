9c0ea6f3e6c0c795bce3953e66d7c0d72fd4cbd1
omwami <ianalpha@protonmail.com>
2023-03-23 15:35:32 +0300

    betty fixes

--- monty.h
+++ monty.h
@@ -17,9 +17,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 extern stack_t *stack;
 /**
@@ -32,8 +32,8 @@ extern stack_t *stack;
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
