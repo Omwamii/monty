c95b5daee7997b26c3a96151fe7b397c1890eb93
omwami <ianalpha@protonmail.com>
2023-03-24 13:28:59 +0300

    fixed swap

--- stack_fns1.c
+++ stack_fns1.c
@@ -106,7 +106,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *swap = NULL;
 
-	if ((*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
