1704513d3e226c16f0b4a8497f3a90c8c03b5d67
omwami <ianalpha@protonmail.com>
2023-03-24 13:35:11 +0300

    fixed mod

--- stack_fns2.c
+++ stack_fns2.c
@@ -95,6 +95,12 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	value = ((*stack)->next->n) % ((*stack)->n);
 	(*stack)->next->n = value;
 	pop(stack, line_number);
